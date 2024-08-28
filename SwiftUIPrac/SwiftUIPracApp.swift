a45d7ebd6cbb9c203858bf513b673de1e6390127
NORIKIM <jinagold.k@gmail.com>
2024-08-28 13:39:51 +0900

    프로젝트 생성

---
+++ SwiftUIPrac/SwiftUIPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracApp.swift
+//  SwiftUIPrac
+//
+//  Created by 인터웍스_맥북 on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
